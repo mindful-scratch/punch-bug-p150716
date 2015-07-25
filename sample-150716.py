12a8e29c998e3c28986fe0630543979252425fff
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2015-07-25 11:36:13 +0900

    🚧加速度を0〜100で出力するようにした

--- sample-150716.py
+++ sample-150716.py
@@ -4,6 +4,12 @@ import math
 adxl345 = ADXL345()
     
 while True:
-	axes = adxl345.getAxes(True)
-	a = math.sqrt( axes['x']**2 + axes['y']**2) * 100
-	print "%.0f" % ( a )
+    axes = adxl345.getAxes(True)
+    a = math.sqrt( axes['x']**2 + axes['y']**2) * 100
+
+    if a < 10:
+        a = 0
+    elif 100 < a:
+        a = 100
+
+    print "%.0f" % ( a )
