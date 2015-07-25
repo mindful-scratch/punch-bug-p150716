7ad029b7970054ef7cd3a71c64379a7e443814d7
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2015-07-25 12:03:04 +0900

    🚧とりあえずステートを「停止」「ゆっくり動いている」「速く動いている」の3つにしてみた

--- sample-150716.py
+++ sample-150716.py
@@ -1,15 +1,34 @@
 from adxl345 import ADXL345
 import math
+import httplib
   
 adxl345 = ADXL345()
-    
+
+STATUS = (
+    STOP,
+    MOVING_SLOWLY,
+    MOVING_FAST
+) = range(0, 3)
+
+status = STOP
+
 while True:
     axes = adxl345.getAxes(True)
     a = math.sqrt( axes['x']**2 + axes['y']**2) * 100
 
     if a < 10:
         a = 0
+        if status is not STOP:
+            status = STOP
+            print "stopped"
     elif 100 < a:
         a = 100
+        if status is not MOVING_FAST:
+            status = MOVING_FAST
+            print "moving fast"
+    else:
+        if status is not MOVING_SLOWLY:
+            status = MOVING_SLOWLY
+            print "moving slowly"
 
-    print "%.0f" % ( a )
+    # print "%.0f" % ( a )
